85902b392db56c3579886d2dafaa388875ad8f31
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-08-10 14:24:15 -0500

    Heroku production -M main :crown:

--- server/index.js
+++ server/index.js
@@ -3,10 +3,14 @@ import morgan from 'morgan';
 import { Server as SocketServer } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url';
 
 import {PORT} from './config.js';
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 const server = http.createServer(app);
 const io = new SocketServer(server, {
     cors: {
@@ -25,8 +29,10 @@ io.on('connection', (socket) => {
             body: message,
             from: socket.id
         });
-    })
-})
+    });
+});
+
+app.use(express.static(join(__dirname, '../client/build')));
 
 server.listen(PORT);
 console.log('listening on port ', + PORT);
